d33d42955962080ea9a2683d8b5be797bdea19e3
hemnath-28 <hemnath0628@gmail.com>
2026-04-13 21:32:02 +0530

    Two sum sorted approach using two pointer

---
+++ q167.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n=len(numbers)
+        left=0
+        right=n-1
+        while left<right:
+            fsum=numbers[left]+numbers[right]
+            if fsum==target:
+                return [left+1,right+1]
+            elif fsum>target:
+                right-=1
+            else:
+                left+=1
+lc=Solution()
+ans=lc.twoSum([-1,0],-1)
+print(ans)
