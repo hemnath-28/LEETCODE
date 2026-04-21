6be6c4a5fdf610f5df8af7c6b2ca49e030bfd1ce
hemnath-28 <hemnath0628@gmail.com>
2026-04-21 18:33:58 +0530

    optimized dict length

--- q904.py
+++ q904.py
@@ -4,25 +4,45 @@ class Solution(object):
         :type fruits: List[int]
         :rtype: int
         """
-
         start=0
-        end=0
-        n=len(fruits)
-        hashmap={}
         maxlen=0
-        while end<n:
-            hashmap[fruits[end]]=hashmap.get(fruits[end],0)+1
-            hashlen=len(hashmap)
+        hashmap={}
+        n=len(fruits)
+        hashlen=0
+        for end in range(n):
+            if fruits[end] not in hashmap:
+                hashmap[fruits[end]]=1
+                hashlen+=1
+            else:
+                hashmap[fruits[end]]+=1
             while hashlen>2:
                 hashmap[fruits[start]]-=1
                 if hashmap[fruits[start]]==0:
-                    hashlen-=1
                     del hashmap[fruits[start]]
+                    hashlen-=1
                 start+=1
-            
-            maxlen=max(maxlen,end-start+1)
-            end+=1
+            maxlen=max(end-start+1,maxlen)
         return maxlen
+        
+
+        # start=0
+        # end=0
+        # n=len(fruits)
+        # hashmap={}
+        # maxlen=0
+        # while end<n:
+        #     hashmap[fruits[end]]=hashmap.get(fruits[end],0)+1
+        #     hashlen=len(hashmap)
+        #     while hashlen>2:
+        #         hashmap[fruits[start]]-=1
+        #         if hashmap[fruits[start]]==0:
+        #             hashlen-=1
+        #             del hashmap[fruits[start]]
+        #         start+=1
+            
+        #     maxlen=max(maxlen,end-start+1)
+        #     end+=1
+        # return maxlen
     
 lc=Solution()
 fruits = [1,2,3,2,2]
