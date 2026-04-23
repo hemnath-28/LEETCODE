4f987ba58be44ac4059ed92e164ba7624c200d8c
hemnath-28 <hemnath0628@gmail.com>
2026-04-23 18:42:15 +0530

    Reverse Between One pass

---
+++ q92.py
@@ -0,0 +1,77 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        """
+        :type head: Optional[ListNode]
+        :type left: int
+        :type right: int
+        :rtype: Optional[ListNode]
+        """
+
+       
+        if not head or left==right:
+            return head
+        dummy=ListNode(0,head)
+        curr=dummy
+        count=0
+        start=None
+        while count<left:
+            start=curr
+            curr=curr.next
+            count+=1
+        temp=curr
+        finish=curr
+        prev=None
+        front=None
+        while count<=right:
+            front=temp.next
+            temp.next=prev
+            prev=temp
+            temp=front
+            count+=1
+        start.next=prev
+        finish.next=front
+        return dummy.next
+
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+   
+               
+            
+            
