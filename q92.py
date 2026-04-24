bf6c3b2ec9e3b555f4d40da008bc2a7c94ce0a5c
hemnath-28 <hemnath0628@gmail.com>
2026-04-24 18:45:35 +0530

    Swapping techniques

--- q92.py
+++ q92.py
@@ -37,7 +37,38 @@ class Solution(object):
         finish.next=front
         return dummy.next
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        """
+        :type head: Optional[ListNode]
+        :type left: int
+        :type right: int
+        :rtype: Optional[ListNode]
+        """
+
+       
+        if not head or left==right:
+            return head
+        dummy=ListNode(0,head)
+        prev = dummy
+
+        for _ in range(left - 1):
+            prev = prev.next
         
+        curr=prev.next
+        for _ in range(right-left):
+            nextnode=curr.next
+            curr.next=nextnode.next
+            nextnode.next=prev.next
+            prev.next=nextnode
+        
+        return dummy.next
+           
         
         
 
